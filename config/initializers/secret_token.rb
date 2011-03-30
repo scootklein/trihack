0e352d2d212d92c53163dee489bf30c22a390eef
Scott Klein <scootklein@gmail.com>
2011-03-29 18:19:33 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trihack::Application.config.secret_token = 'a75e6ac102c70b88eb959f3dfcdf4bd2a6c6620ce802bc5e5bc57c1067261b62709b967a5ece11c11415eb236e031db5bf57e320a4ecb5ea7964e3c9dc5b9783'
